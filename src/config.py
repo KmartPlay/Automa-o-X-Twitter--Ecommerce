0dccafa661fa34464680055d2e7d07a6588d4566
Gomes <134858884+KmartPlay@users.noreply.github.com>
2025-09-30 21:00:01 -0300

    config.py

---
+++ src/config.py
@@ -0,0 +1,14 @@
+# config.py
+# Arquivo central para guardar algumas variaveis do bot.
+
+#Tempo para enviar o proximo tweet - para não ser bloqueado pela API por SPAM
+tem_proximo = 190
+
+#Quantidade de "Produtos" que o bot vai pegar do ecommerce
+qtd_itens = 9
+
+#Para chamara API colocar True
+chamar_api_x = True
+
+
+UrlLink = "LINK DO ECOMMERCE"
